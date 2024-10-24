8c8dd218c4d0554d0edb6b0dacad25dbb345a429
KingLeonYT <tannersrn1@gmail.com>
2024-10-24 09:13:26 -0500

    Parte 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] pronombres2 = new string[3];
+string[] pronombres = { "tu", "yo", "Nosotros" };
+for (int i = 0; i < pronombres2.Length; i++)
+{
+    Console.WriteLine($"Ingrese el nombre {i+1}");
+    pronombres2[i] = Console.ReadLine();
+}
+for (int i = 0;i < pronombres2.Length;i++)
+{ Console.WriteLine(pronombres2[i]); }
+foreach (string s in pronombres)
+{
+    Console.WriteLine(s);
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una cantidad de elementos: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] numeros = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                numeros[i] = rnd.Next(10, 99 + 1);
+            }
+            string todos=string.Join(",", numeros);
+            Console.WriteLine($"Numeros{ todos}");
+        }
+    }
+}
